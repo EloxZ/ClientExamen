8181e90c5edf9a9a99f2a000b97e4ddebea069d1
EloxZ <45355879+EloxZ@users.noreply.github.com>
2022-01-20 20:06:20 +0100

    Prueba

---
+++ cogertoken.php
@@ -0,0 +1,29 @@
+<?php
+
+$provider = new \League\OAuth2\Client\Provider\GenericProvider([
+    'clientId'                => 'application',    // The client ID assigned to you by the provider
+    'clientSecret'            => 'secret',    // The client password assigned to you by the provider
+    'redirectUri'             => 'https://google.es/',
+    'urlAuthorize'            => 'https://service.example.com/authorize',
+    'urlAccessToken'          => 'https://examenserver.herokuapp.com/oauth/token',
+    'urlResourceOwnerDetails' => 'https://service.example.com/resource'
+]);
+
+try {
+
+    // Try to get an access token using the resource owner password credentials grant.
+    $accessToken = $provider->getAccessToken('password', [
+        'username' => 'pedroetb',
+        'password' => 'password'
+    ]);
+
+    $_SESSION['oauth2state'] = $provider->getState();
+
+} catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+
+    // Failed to get the access token
+    exit($e->getMessage());
+
+}
+
+?>
---
+++ vertoken.php
@@ -0,0 +1,7 @@
+<?php 
+
+if (isset($_SESSION['oauth2state'])) {
+    var_dump($_SESSION['oauth2state']);
+}
+
+?>
