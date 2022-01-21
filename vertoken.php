43ff534e4afe8b535d8b45b127472f12f3eb9ed4
EloxZ <45355879+EloxZ@users.noreply.github.com>
2022-01-21 15:04:01 +0100

    Update vertoken.php

--- vertoken.php
+++ vertoken.php
@@ -2,13 +2,14 @@
 session_start();
 
 if (isset($_SESSION['token'])) {
-    var_dump($_SESSION['token']);
-    $tokenArray = (array) $_SESSION['token'];
-    echo $tokenArray['accessToken'];
+    
+    $tokenJson = json_encode($_SESSION['token']);
+    var_dump($tokenJson);
+    echo $tokenJson->accessToken;
 
     $ch = curl_init('https://examenserver.herokuapp.com/oauth/auth');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, 'Authorization: Bearer '.$tokenArray['accessToken']);
+    curl_setopt($ch, CURLOPT_HEADER, 'Authorization: Bearer '.$tokenJson->accessToken);
     $data = curl_exec($ch);
     echo $data;
     curl_close ($ch);
