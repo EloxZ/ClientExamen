6c192aa95df0444977b9d67cc217f6ef9b1a3232
EloxZ <eloyway@gmail.com>
2022-01-21 19:53:24 +0100

    Final

--- trabajo/includes/mapa.php
+++
@@ -1,5 +0,0 @@
-<section class="container">
-    <div class="header-opciones">
-        <a href="incidencias.php" class="btn btn-primary">Mapa de incidencias</a>
-    </div>
-</section>
--- trabajo/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-
-unset($_SESSION['login']);
-unset($_SESSION['admin']);
-unset($_SESSION['usuario']);
-unset($_SESSION['google_login']);
-
-header('Location: /index.php');
