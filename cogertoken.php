44aa231d19c90cda1a6db97435a76cb434ccb3dd
EloxZ <45355879+EloxZ@users.noreply.github.com>
2022-01-21 14:04:32 +0100

    Update cogertoken.php

--- cogertoken.php
+++ cogertoken.php
@@ -1,33 +1,33 @@
 <?php
 
+if (!isset($_SESSION['token'])) {
+    try {
+        $post = [
+            'grant_type'   => 'password',
+            'username' => 'pedroetb',
+            'password' => 'password'
+        ];
+        
+        $headers = [
+            'Authorization: Basic YXBwbGljYXRpb246c2VjcmV0',
+            'Content-Type: application/x-www-form-urlencoded'
+        ];
 
-try {
-    $post = [
-        'grant_type'   => 'password',
-        'username' => 'pedroetb',
-        'password' => 'password'
-    ];
-    
-    $headers = [
-        'Authorization: Basic YXBwbGljYXRpb246c2VjcmV0',
-        'Content-Type: application/x-www-form-urlencoded'
-    ];
+        $ch = curl_init('https://www.examenserver.herokuapp.com/oauth/token');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $token = curl_exec($ch);
 
-    $ch = curl_init('https://www.examenserver.herokuapp.com/oauth/token');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    $token = curl_exec($ch);
+        $_SESSION['token'] = $token;
+        var_dump($token);
+        curl_close ($ch);
 
-    $_SESSION['token'] = $token;
-    var_dump($token);
-    curl_close ($ch);
+    } catch (Exception $e) {
 
-} catch (Exception $e) {
-
-    // Failed to get the access token
-    echo $e->getMessage();
-
+        // Failed to get the access token
+        echo $e->getMessage();
+    }
 }
 
 ?>
